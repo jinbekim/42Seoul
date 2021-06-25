1fb7b1a6682020e56c0ce9089ea94d2fc30cee9e
jinbekim <jinbekim@student.42seoul.kr>
2021-06-25 12:48:09 +0900

    second

--- cpp00/ex01/phonebook.cpp
+++ cpp00/ex01/phonebook.cpp
@@ -1,19 +1,47 @@
 #include "phonebook.hpp"
 
-static void	add_func(void)
+static void	add_func(phonebook (*data)[8])
 {
+	int	i = 0;
+	phonebook tmp;
 	std::string greeting_msg = "Please enter in the following order.";
-	std::string input_list = "first name, last name, nickname, \
-	login, postal address, email address, phone number, birthday date, favorite \
-	meal, underwear color and darkest secret";
-	std::cout << input_list << std::endl;
 
+	while (i < 8 && (*data)[i].filled)
+		i++;
+	if (i == 8)
+		tmp = (*data)[0];
+	else
+		tmp = (*data)[i];
+	std::cout << "what's your first and last name?" << std::endl;
+	std::cin >> tmp.first_name >> tmp.last_name;
+	std::cout << "what's your nick name?" << std::endl;
+	std::cin >> tmp.nick_name;
+	std::cout << "what's your login?" << std::endl;
+	std::cin >> tmp.login;
+	std::cout << "what's your phone number?" << std::endl;
+	std::cin >> tmp.phone_num;
+	std::cout << "what's your birthday_date?" << std::endl;
+	std::cin >> tmp.birthday_date;
+	std::cout << "what's your favorite_meal?" << std::endl;
+	std::cin >> tmp.favorite_meal;
+	std::cout << "what's your underwear_color?" << std::endl;
+	std::cin >> tmp.underwear_color;
+	std::cout << "what's your darkest_secret?" << std::endl;
+	std::cin >> tmp.darkest_secret;
+	tmp.filled = 1;
+	std::cout << "your data saved succesfully" << std::endl;
+	tmp.show_info();
 }
 
-static void	search_func(void)
+static void	search_func(phonebook (*data)[8])
 {
-	// index, first name, last name and nickname.
+	int	i = 0;
 
+	while (i < 8 && (*data)[i].filled)
+	{
+
+		i++;
+	}
 }
 
 int	main(void)
@@ -21,15 +49,16 @@ int	main(void)
 	std::string	CMD;
 	std::string greeting_msg = "Please enter command. ADD, SEARCH or EXIT";
 	std::string goodbye_msg = "Good bye!, you will lose everything.  :(";
+	phonebook	data[8];
 
-	while (CMD.compare("EXIT") || CMD.compare("exit"))
+	while (CMD.compare("EXIT") || CMD != "exit")
 	{
 		std::cout << greeting_msg << std::endl;
 		std::getline(std::cin, CMD);
 		if (!CMD.compare("ADD") || CMD == "add")
-			add_func();
+			add_func(&data);
 		else if (!CMD.compare("SEARCH") || !CMD.compare("search"))
-			search_func();
+			search_func(&data);
 		else
 			continue ;
 	}
--- cpp00/ex01/phonebook.hpp
+++ cpp00/ex01/phonebook.hpp
@@ -2,14 +2,48 @@
 # define PHONE
 
 # include <iostream>
+# include <iomanip>
 # include <string>
 
-
-class	data_base{
-	private :
-
+class	phonebook{
 	public :
+		std::string	first_name;
+		std::string	last_name;
+		std::string	nick_name;
+		std::string	login;
+		std::string	postal_address;
+		std::string	email;
+		std::string	phone_num;
+		std::string	birthday_date;
+		std::string	favorite_meal;
+		std::string	underwear_color;
+		std::string	darkest_secret;
+		bool	filled;
+		void	show_index(int i);
+		void	show_info();
+		// phonebook();
+		// ~phonebook();
+};
 
+void	phonebook::show_index(int i)
+{
+	std::cout << std::setw(10) << i << "|";
+	std::cout << std::setw(10) << first_name << "|";
+	std::cout << std::setw(10) << last_name << "|";
+	std::cout << std::setw(10) << nick_name << std::endl;
 }
-
+void	phonebook::show_info()
+{
+	std::cout << first_name << std::endl;
+	std::cout << last_name << std::endl;
+	std::cout << nick_name << std::endl;
+	std::cout << login << std::endl;
+	std::cout << postal_address << std::endl;
+	std::cout << email << std::endl;
+	std::cout << phone_num << std::endl;
+	std::cout << birthday_date << std::endl;
+	std::cout << favorite_meal << std::endl;
+	std::cout << underwear_color << std::endl;
+	std::cout << darkest_secret << std::endl;
+}
 #endif
