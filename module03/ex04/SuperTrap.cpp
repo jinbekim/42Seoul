5a620a9509fdd0376515305d6fb04f7dce1db413
jinbekim <jinbekim@student.42seoul.kr>
2021-12-02 16:58:24 +0900

    fixed: wrong inheritance

--- module03/ex04/SuperTrap.cpp
+++ module03/ex04/SuperTrap.cpp
@@ -1,11 +1,12 @@
 # include "SuperTrap.hpp"
 
-SuperTrap::SuperTrap(const std::string &name): NinjaTrap(name), FlagTrap(name)
+SuperTrap::SuperTrap(const std::string &name)
+	: ClapTrap(name), NinjaTrap(name), FlagTrap(name)
 {
-	this->Hit_points = this->FlagTrap::Hit_points;
-	this->Max_hit_points = this->FlagTrap::Max_hit_points;
-	this->Energy_points = this->NinjaTrap::Energy_points;
-	this->Max_energy_points = this->NinjaTrap::Max_energy_points;
+	this->Hit_points = 100; //this->FlagTrap::Hit_points;
+	this->Max_hit_points = 100; //this->FlagTrap::Max_hit_points;
+	this->Energy_points = 120; //this->NinjaTrap::Energy_points;
+	this->Max_energy_points = 120; //this->NinjaTrap::Max_energy_points;
 	this->Level = 1;
 	this->Name = name;
 	this->Melee_attack_damage = this->NinjaTrap::Melee_attack_damage;
@@ -13,7 +14,8 @@ SuperTrap::SuperTrap(const std::string &name): NinjaTrap(name), FlagTrap(name)
 	this->Armor_damage_reduction = this->FlagTrap::Armor_damage_reduction;
 }
 
-SuperTrap::SuperTrap(const SuperTrap &ref): NinjaTrap(ref.Name), FlagTrap(ref.Name)
+SuperTrap::SuperTrap(const SuperTrap &ref)
+	: ClapTrap(ref.Name), NinjaTrap(ref.Name), FlagTrap(ref.Name)
 {
 	ClapTrap::copy(ref);
 }
